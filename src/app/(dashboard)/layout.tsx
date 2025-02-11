ca510af091c0f950f8dec6686158a392c7ed0358
Chi Xu <xuchi007@hotmail.com>
2025-02-11 18:36:06 +1300

    side menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Menu from '@/components/Menu';
+import Navbar from '@/components/Navbar';
 
 export default function DashboardLayout({
   children,
@@ -9,21 +9,15 @@ export default function DashboardLayout({
   return (
     <div className="flex h-screen">
       {/* Left */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        <Link href={'/'} className='flex items-center justify-center gap-2'>
-          <Image
-            src={'/images/logo.png'}
-            alt={'logo'}
-            width={32}
-            height={32}
-          />
-          <span className='hidden lg:block'>School Management</span>
-        </Link>
-      </div>
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
-        Right
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Menu />
       </div>
       {/* Right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]
+       bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
